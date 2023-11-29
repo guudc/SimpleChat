b9683c41fc68a56cee7dfb90249a9e897a16cf0f
guudc <65718626+guudc@users.noreply.github.com>
2023-11-29 15:23:21 +0100

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,4 +1,4 @@
 const OpenAI  = require('openai')
-const api_key = "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"
+const api_key = "<OPENAI_API_KEY>"
 const openai = new OpenAI({ apiKey: api_key })
 exports.openai = openai
